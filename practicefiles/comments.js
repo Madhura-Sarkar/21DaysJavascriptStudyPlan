48de2d43d575384cf62d876008a603fb7e801f30
Madhura Sarkar <madhurasarkar0@gmail.com>
2026-02-15 22:08:20 +0530

    Added practice files

---
+++ practicefiles/comments.js
@@ -0,0 +1,8 @@
+/* A Multiline Comment --            
+Define a variable of            
+string type and assign            
+value to it*/    
+
+// single line comment
+
+console.log("Hello World");
---
+++ practicefiles/initialise2.js
@@ -0,0 +1,3 @@
+// const age; is not allowed because it must be initialized at the time of declaration
+const age = 18;
+console.log(age);
