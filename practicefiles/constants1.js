9e586b52867f4fb9511e011fe8c9b859f0b6c805
Madhura Sarkar <madhurasarkar0@gmail.com>
2026-02-15 22:23:07 +0530

    Added constants1.js file

---
+++ practicefiles/constants1.js
@@ -0,0 +1,8 @@
+// Create a constant called day and assign Monday to it.
+// Print day to the console.
+// Reassign day with Tuesday. What happens?
+// error occured
+
+const day = "Monday";
+console.log(day);
+// day = "tuesday"; //error 
