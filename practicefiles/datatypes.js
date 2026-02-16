df09ab34908fdbb762cd049957e5a043059fd6dc
Madhura Sarkar <madhurasarkar0@gmail.com>
2026-02-16 23:47:11 +0530

    create datatypes.js

---
+++ practicefiles/datatypes.js
@@ -0,0 +1,22 @@
+// Create a variable num and assign it the value 42. Create another variable str and assign it the value "Hello, World!". Create a third variable bool and assign it the value true. Log the values of all three variables to the console.
+let num = 42;
+let string = "Hello, world!";
+let bool = true;
+console.log(num);
+console.log(string);
+console.log(bool);
+
+
+// Create a variable x and assign it the value null. Log the value of x to the console.
+let x = null;
+console.log(x);
+
+
+// Create a variable y but do not assign it a value. Log the value of y to the console. What is the value of y?
+let y;
+console.log(y);
+
+
+// Create a variable z and assign it the value undefined. Log the value of z to the console.
+let z = undefined;
+console.log(z);
