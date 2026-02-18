7e89707d92df82e74719e705f1e70946a9e8a7bf
Madhura Sarkar <madhurasarkar0@gmail.com>
2026-02-18 21:38:50 +0530

    create shortcircuit.js

---
+++ practicefiles/shortcircuit.js
@@ -0,0 +1,24 @@
+// What will be the output of the following program?
+// let y = x || 5;
+// console.log(y);
+// Output: ReferenceError: x is not defined
+
+
+
+
+
+// What will be the output of the following program?
+let X = "hello";
+let Y = X && "world";
+console.log(Y);
+// Output: world
+
+
+
+
+
+// What will be the output of the following program?
+let x1 = 0;
+let y1 = x1 || "default value";
+console.log(y1);
+// Output: default value
