30f06277447c539a0397b2d5a8f4664239ca4575
Madhura Sarkar <madhurasarkar0@gmail.com>
2026-02-18 21:31:40 +0530

    create ternaryoperator.js

---
+++ practicefiles/ternaryoperator.js
@@ -0,0 +1,29 @@
+// Write a ternary operator that checks if a variable name is "John". If it is, assign the value "Hello, John" to a variable result, otherwise assign the value "Hello, stranger".
+let name = "John";
+let result = (name === "John") ? "Hello, John" : "Hello, stranger";
+console.log(result);
+
+
+
+
+
+// Write a ternary operator that checks if a variable isLoggedIn is true. If it is, assign the value "Welcome back!" to a variable result, otherwise assign the value "Please log in to continue".
+let isLoggedIn = true;
+let result2 = (isLoggedIn) ? "Welcome back!" : "Please log in to continue";
+console.log(result2);
+
+
+
+
+
+// Covert the following to a ternary statement:
+// let age = 20;
+// let result;
+// if (age >= 18) {
+//   result = "Adult";
+// } else {
+//   result = "Child";
+// }
+let age = 20;
+let result3 = (age >=18) ? "you are an " + "Adult" : "Child";
+console.log(result3);
