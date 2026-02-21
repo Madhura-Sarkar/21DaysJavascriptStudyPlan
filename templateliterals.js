c77e1c071849ec2504ae9904dc318c33c717bb14
Madhura Sarkar <madhurasarkar0@gmail.com>
2026-02-21 23:50:21 +0530

    create templateliterals.js

---
+++ templateliterals.js
@@ -0,0 +1,11 @@
+// Template Literals are a modern way to write strings in JavaScript 
+// They allow for easier string interpolation and multi-line strings
+
+// let text = "Hello, 
+//     World!"
+//error 
+
+let text1 = `Hello,
+    World!`
+    console.log(text1);
+    // works fine
