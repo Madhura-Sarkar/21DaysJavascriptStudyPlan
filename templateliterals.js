f19b30cb96b81a4466e0901f96384cd1763df4c2
Madhura Sarkar <madhurasarkar0@gmail.com>
2026-02-21 23:55:46 +0530

    add variable insertion, html templates to templateliterals.js

--- templateliterals.js
+++ templateliterals.js
@@ -9,3 +9,36 @@ let text1 = `Hello,
     World!`
     console.log(text1);
     // works fine
+
+
+
+
+
+    // variable insertion 
+    // this is the most important feature of template literals
+    let myName = "Madhura";
+    console.log(`Hello, ${myName}!`);
+
+    //${} is used to insert variables or expressions into the string
+
+    // even calculations
+    let a = 5;
+    let b = 10;
+    console.log(`The sum is ${a + b}.`);
+
+
+
+
+
+    //HTML templates
+    let product = "Laptop";
+    let price = 50000;
+    let card = `
+    <div>
+        <h2>${product}</h2>
+        <p>Price: $${price}</p>
+    </div>
+    `;
+    console.log(card);
+
+    // useful for creating dynamic HTML content in JavaScript applications, DOM manipulation, especially when working with frameworks like React or Vue.js.
