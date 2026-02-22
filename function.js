22260085d27e6fc21b348b8fc28a34bc138add30
Madhura Sarkar <madhurasarkar0@gmail.com>
2026-02-22 23:13:50 +0530

    create function.js

---
+++ function.js
@@ -0,0 +1,16 @@
+function doMath(num){
+    num = Number(num);
+    if (num){
+        var NewNumber = (num +100)/3;
+        return NewNumber;
+    } else {
+        return false;
+    }
+}
+
+var customNum = doMath(343);
+console.log(customNum);
+var customNum = doMath("Tina");
+console.log(customNum);
+
+
