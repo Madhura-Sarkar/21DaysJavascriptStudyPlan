2d43a44d8a48579018d261419021c63cf15fd2e0
Madhura Sarkar <madhurasarkar0@gmail.com>
2026-02-23 21:15:03 +0530

    create string.js in practicefiles

---
+++ practicefiles/string.js
@@ -0,0 +1,22 @@
+// Declare a variable str and assign it a string value of "Hello, World!".
+let str = "Hello, World!";
+console.log(str);
+
+// Use the length property to find the length of the str variable.
+console.log(str.length);
+
+// Use the indexOf() method to find the index of the first occurrence of the letter "o" in the str variable.
+console.log(str.indexOf("o"));
+
+// Use the concat() method to concatenate the str variable with the string " How are you?".
+let newStr = str.concat(" How are you?");
+console.log(newStr);
+
+// Use the toUpperCase() method to convert the str variable to all uppercase letters.
+console.log(str.toUpperCase());
+
+// Use a template literal to concatenate the values of two variables (firstName and lastName) into a full name.
+let firstName = "Madhura";
+let lastName = "Sarkar";
+let fullName = `${firstName} ${lastName}`;
+console.log(fullName);
