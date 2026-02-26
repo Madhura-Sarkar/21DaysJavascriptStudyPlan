718aa1649f246f0327a5d07946f98091b13bc316
Madhura Sarkar <madhurasarkar0@gmail.com>
2026-02-26 22:19:10 +0530

    create classes.js

---
+++ classes.js
@@ -0,0 +1,15 @@
+// creating class without constructor 
+class Car {
+    drive() {
+        console.log("let's drive the car");
+    }
+    brake() {
+        console.log("you step on the brakes");
+    }
+}
+
+let car1 = new Car();
+let car2 = new Car();
+car1.drive();
+car2.brake();
+
