860b2ac9fbf8948eb779d87d0404df18a291952b
Madhura Sarkar <madhurasarkar0@gmail.com>
2026-03-05 22:25:23 +0530

    create coercioncon.js

---
+++ coercioncon.js
@@ -0,0 +1,28 @@
+// const loginDetails = ''
+// const loginID = loginDetails[0]
+// if(loginID !== undefined){
+//     console.log('Allow user to login');
+// } else {
+//     console.log('Auth Failed')
+// }
+
+const loginDetails = ' '
+const loginID = loginDetails[0]
+if(loginID !== undefined){
+    console.log('Allow user to login');
+} else {
+    console.log('Auth Failed')
+}
+
+//values that interpret as false: 0, false, '', null, undefined
+
+
+
+
+
+// conversions
+var age = window.prompt("enter age");
+console.log("Happy birthday")
+age = Number(age);
+age += 1;
+console.log(age);
