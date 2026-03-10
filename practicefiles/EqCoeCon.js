9d12198787cec7ee661d075af7320550fec95897
Madhura Sarkar <madhurasarkar0@gmail.com>
2026-03-10 20:11:05 +0530

    create EqCoeCon.js in practicefiles

---
+++ practicefiles/EqCoeCon.js
@@ -0,0 +1,22 @@
+console.log(5 == "5"); // Output:
+console.log(5 === "5"); // Output:
+console.log(5 != "5"); // Output:
+console.log(5 !== "5"); // Output:
+
+
+console.log("5" + 5); // Output:
+console.log("5" - 5); // Output:
+console.log(true + true); // Output:
+console.log(false + false); // Output:
+console.log(null + 5); // Output:
+console.log(undefined + 5); // Output:
+
+
+
+// Convert the boolean value true to a string and store it in a variable str.
+const str = String(true);
+console.log(str);
+
+// Convert the number 3.14 to a string and store it in a variable str.
+const str1 = String(3.14);
+console.log(typeof(str1));
