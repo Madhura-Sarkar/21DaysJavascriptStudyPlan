b2f6101da7d8d640e087d7466aed9cd435c5f253
Madhura Sarkar <madhurasarkar0@gmail.com>
2026-03-21 22:51:36 +0530

    create callback.js

---
+++ callback.js
@@ -0,0 +1,24 @@
+const userLeft = false
+const userWatchingCatMeme = false
+function watchTutorialCallback(callback, errorCallback) {
+    if (userLeft) {
+        errorCallback({
+            name: 'User Left',
+            message: ':('
+        })
+    }
+    else if(userWatchingCatMeme){
+        errorCallback({
+            name: 'User Watching Cat Meme',
+            message: 'webDevSimplified < Cat'
+        })
+    } else {
+        callback('Thumbs up and subscribe')
+    }
+}
+
+watchTutorialCallback((message) =>{
+    console.log('success: ' + message)
+}, (error) => {
+        console.log(error.name + '' + error.message)
+})
